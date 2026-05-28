6aec126b8753d2cf3c937eee20d54edec7d7593e
Annu Priya <annuchy8265@gmail.com>
2026-05-28 22:46:07 +0530

    Bootcamp progress

---
+++ laranssmall.java
@@ -0,0 +1,17 @@
+public class laranssmall {
+    public static void main(String[] args) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int arr[]={43,1,3,2,3,4,2,34,620};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Max Number :"+max);
+        System.out.println("Minimum Number :"+min);
+    }
+}
