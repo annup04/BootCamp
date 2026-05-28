6d0f7a78cdbd5a60c74b115190f5d15778b2e02a
Annu Priya <annuchy8265@gmail.com>
2026-05-28 22:54:03 +0530

    Bootcamp progress

---
+++ matrixdiagnoldiff.java
@@ -0,0 +1,14 @@
+public class matrixdiagnoldiff {
+    public static void main(String[] args) {
+        int leftDiagnol=0;
+        int rightDiagnol=0;
+        int martrix[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int  n=martrix.length;
+        for(int i=0;i<martrix.length;i++){
+            leftDiagnol+=martrix[i][i];
+            rightDiagnol+=martrix[i][n-i-1];
+        }
+        System.out.println(Math.abs(rightDiagnol-leftDiagnol));
+    }
+    
+}
